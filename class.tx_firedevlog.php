05a92e610adfe90d381b83b8b0d7ec11febc0bd1
ischmittis <ischmittis@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-08-03 18:12:55 +0000

    03.08.2008 Ingo Schmitt <is@marketing-factory.de> * Initial Upload to forge and TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fire_devlog/trunk@10918 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ class.tx_firedevlog.php
@@ -0,0 +1,115 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2008 Marketing Factory TYPO3 dev Team <typo3@marketing-factory.de> 
+*  All rights reserved
+*
+*  This script is part of the Typo3 project. The Typo3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+ 
+ 
+if(!class_exists('FirePHP'))
+{
+	require_once(t3lib_extMgm::extPath('fire_devlog').'lib/FirePHP.class.php');
+	require_once(t3lib_extMgm::extPath('fire_devlog').'lib/fb.php');
+}
+/**
+ * Fire-Devlog Hauptklasse
+ */
+class tx_firedevlog
+{
+	function devLog($logArr)
+	{
+		//Check the config
+		$staticConf = unserialize ($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['fire_devlog']);
+		if (!is_array ($staticConf)) return;
+		$enableBELogging = $staticConf['enableBELogging'];
+		$iprange		 = $staticConf['iprange'];
+		
+		//Logging only if BE User is logged in?
+		if($enableBELogging && !$GLOBALS['BE_USER']->user)
+		return;
+		
+		//Logging only for an IP Range?
+		if('' != $iprange)
+		{
+			if(!t3lib_div::cmpIP(t3lib_div::getIndpEnv('REMOTE_ADDR'), trim($iprange)))
+			return;
+		}
+		//return t3lib_div::cmpIP(t3lib_div::getIndpEnv('REMOTE_ADDR'), trim($value)) ? TRUE : FALSE;
+		
+		//Holds the severity information
+		$severity = array (
+			'0'  => FirePHP::LOG,
+			'1'  => FirePHP::INFO,
+			'2'  => FirePHP::WARN,
+			'3'  => FirePHP::ERROR,
+			'-1' => FirePHP::INFO,
+		);	
+		
+		fb(date('d.m.Y H:i:s').' by '.$logArr['extKey'].': '.$logArr['msg'] ,$severity[$logArr['severity']]);
+		if (!empty($logArr['dataVar'])) {
+			fb($this->getPrintable ($logArr['dataVar']) . chr(10));
+		}
+	}
+	
+	/**
+	 * Returns a suitable form of a variable (be it a string, array, object ...) for logfile output
+	 * 
+	 * @param	mixed	$var: The variable
+	 * @param	integer	$spaces: Number of spaces to add before a line
+	 * @return	string	text output
+	 */
+	function getPrintable($var, $spaces=4) {
+		if ($spaces > 100) return;
+		$out = '';
+		if (is_array ($var)) {
+			foreach ($var as $k=>$v) {
+				if (is_array ($v)) {
+					$out .= str_repeat (' ',$spaces).$k.' => array ('.chr(10).$this->getPrintable($v, $spaces+3).str_repeat (' ',$spaces).')'.chr(10);
+				} else {
+					if (is_object($v)) {
+						$out .= str_repeat (' ',$spaces).$k.' => object: '.get_class ($v).chr(10);
+					} else {
+						$out .= str_repeat (' ',$spaces).$k.' => '.$v.chr(10);
+					}
+				}
+			}
+			return $out;
+		} else {
+			if (is_object($var)) {
+				$out .= str_repeat (' ',$spaces).' [ OBJECT: '.strtoupper(get_class ($var)).' ]:'.chr(10);
+				if (is_array (get_object_vars ($var))) {
+					foreach (get_object_vars ($var) as $objVarName => $objVarValue) {
+						if (is_array ($objVarValue) || is_object ($objVarValue)) {
+							$out .= str_repeat (' ',$spaces). $objVarName . ' => '.chr(10);
+							$out .= $this->getPrintable ($objVarValue, $spaces+3);
+						} else {
+							$out .= str_repeat (' ',$spaces). $objVarName . ' => ' .$objVarValue.chr(10);
+						}	
+					}	
+				}
+				$out .=chr(10);
+			} else {
+				$out .= str_repeat (' ',$spaces).'=> '.$var.chr(10);
+			}
+			return $out;
+		}
+	}
+}
+?>
---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "fire_devlog"
+#
+# Auto generated 03-08-2008 20:10
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Fire DevLog',
+	'description' => 'Writes the System DevLog into Firebug',
+	'category' => 'misc',
+	'author' => 'Marketing Factory TYPO3 dev Team',
+	'author_email' => 'typo3@marketing-factory.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.9.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"a447";s:23:"class.tx_firedevlog.php";s:4:"177b";s:21:"ext_conf_template.txt";s:4:"f1e8";s:12:"ext_icon.gif";s:4:"9a17";s:17:"ext_localconf.php";s:4:"e1c3";s:14:"doc/manual.sxw";s:4:"3f3a";s:19:"doc/wizard_form.dat";s:4:"8522";s:20:"doc/wizard_form.html";s:4:"0411";s:21:"lib/FirePHP.class.php";s:4:"0856";s:10:"lib/fb.php";s:4:"3bc4";}',
+	'suggests' => array(
+	),
+);
+
+?>
