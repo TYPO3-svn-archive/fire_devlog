c1f5988e90d6ae08df18f4bbdbc003521702d906
xonx <xonx@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-28 14:55:16 +0000

    Fixed: #13439: Use autoloader instead of require_once()
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fire_devlog/trunk@44397 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,9 +1,6 @@
 <?php
 if (!defined ("TYPO3_MODE")) 	die ("Access denied.");
 
-
-require_once(t3lib_extMgm::extPath('fire_devlog').'class.tx_firedevlog.php');
-
 ob_start();
 
 $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_div.php']['devLog'][$_EXTKEY] = 'EXT:'.$_EXTKEY.'/class.tx_firedevlog.php:tx_firedevlog->devLog';
