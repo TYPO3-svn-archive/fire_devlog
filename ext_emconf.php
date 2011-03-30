193b93150a51c61abab02bca4f67b8bff767ea91
ischmittis <ischmittis@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-30 10:29:27 +0000

    released Version 1.2.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fire_devlog/trunk@45956 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "fire_devlog".
 #
-# Auto generated 28-02-2011 18:43
+# Auto generated 30-03-2011 12:28
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Writes the System DevLog into Firebug',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '1.1.0',
+	'version' => '1.2.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"94bb";s:23:"class.tx_firedevlog.php";s:4:"1ec3";s:16:"ext_autoload.php";s:4:"f89d";s:21:"ext_conf_template.txt";s:4:"0c49";s:12:"ext_icon.gif";s:4:"9a17";s:17:"ext_localconf.php";s:4:"8c08";s:14:"doc/manual.sxw";s:4:"87b8";s:19:"doc/wizard_form.dat";s:4:"8522";s:20:"doc/wizard_form.html";s:4:"0411";s:21:"lib/FirePHP.class.php";s:4:"48f5";s:10:"lib/fb.php";s:4:"818b";}',
+	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"9375";s:23:"class.tx_firedevlog.php";s:4:"1ec3";s:16:"ext_autoload.php";s:4:"f89d";s:21:"ext_conf_template.txt";s:4:"0c49";s:12:"ext_icon.gif";s:4:"9a17";s:17:"ext_localconf.php";s:4:"8c08";s:14:"doc/manual.sxw";s:4:"897c";s:19:"doc/wizard_form.dat";s:4:"8522";s:20:"doc/wizard_form.html";s:4:"0411";s:21:"lib/FirePHP.class.php";s:4:"48f5";s:10:"lib/fb.php";s:4:"818b";}',
 	'suggests' => array(
 	),
 );
